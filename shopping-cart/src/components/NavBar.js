3369a43d0421e0044b25e55f1d63583a21bf9ab6
Jamaal <jjrog16@gmail.com>
2021-11-24 18:16:03 -0500

    Styled nav bar

--- shopping-cart/src/components/NavBar.js
+++ shopping-cart/src/components/NavBar.js
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function NavBar() {
-  const [click, setClick] = useState(true);
   const [cartCount, setCartCount] = useState(0);
-
-  function handleClick() {
-    // When a click happens, set the opposite of the current state of a click
-    setClick(!click);
-  }
-
-  // When an option is clicked in mobile, close the menu
-  function closeMobileMenu() {
-    setClick(false);
-  }
 
   return (
     <>
@@ -26,26 +13,19 @@ function NavBar() {
           <Link to="/" className="navbar-logo">
             Caked Up!
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <MenuIcon /> : <CloseIcon />}
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <ul className="link-container">
             <li className="nav-links">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              <Link to="/" className="nav-links">
                 Home
               </Link>
             </li>
             <li className="nav-links">
-              <Link to="/menu" className="nav-links" onClick={closeMobileMenu}>
+              <Link to="/menu" className="nav-links">
                 Menu
               </Link>
             </li>
             <li className="nav-links">
-              <Link
-                to="/contact"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
+              <Link to="/contact" className="nav-links">
                 Contact Us
               </Link>
             </li>
